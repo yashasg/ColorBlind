47797ea7e53fcb480b3608286592785d7f44207b
Kun Cheng <kunchengsky@gmail.com>
2016-02-27 19:00:40 -0700

    Added a level loader

---
+++ Assets/LunchLevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LunchLevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject[] eatableFoods = GameObject.FindGameObjectsWithTag("eatable");
+        if (eatableFoods.Length <= 0)
+        {
+            Application.LoadLevel(1);
+        }
+	}
+}
