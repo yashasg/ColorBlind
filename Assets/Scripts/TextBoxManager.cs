8e065183dee4f47647281f7bc13149f0245f4d96
Kun Cheng <kunchengsky@gmail.com>
2016-02-27 23:59:49 -0700

    Added a Dialouge scene

---
+++ Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+    public GameObject textBox;
+    public Text theText;
+
+
+    public TextAsset textfile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    //public PlayerController player;
+
+    // Use this for initialization
+    void Start()
+    {
+        //player = FindObjectOfType<PlayerController>();
+
+        if (textfile != null)
+        {
+            textLines = (textfile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine += 1;
+        }
+
+        if (currentLine > endAtLine)
+        {
+            textBox.SetActive(false);
+        }
+	}
+}
---
+++ Assets/Scripts/TextImporter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextImporter : MonoBehaviour {
+
+    public TextAsset textfile;
+    public string[] textLines;
+
+	// Use this for initialization
+	void Start () {
+	    if (textfile != null)
+        {
+            textLines = (textfile.text.Split('\n'));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
