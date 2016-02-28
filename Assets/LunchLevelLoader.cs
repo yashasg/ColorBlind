50b9c8a05103cec16dd3f0a3619db13e657cd194
Kun Cheng <kunchengsky@gmail.com>
2016-02-27 19:53:04 -0700

    Added fading between scenes

--- Assets/LunchLevelLoader.cs
+++ Assets/LunchLevelLoader.cs
@@ -5,15 +5,22 @@ public class LunchLevelLoader : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        GetComponent<Fading>().BeginFade(-1);
 	}
 	
+    IEnumerator ChangeLevel()
+    {
+        float fadeTime = GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(4.0f);
+        Application.LoadLevel(1);
+    }
+
 	// Update is called once per frame
 	void Update () {
         GameObject[] eatableFoods = GameObject.FindGameObjectsWithTag("eatable");
         if (eatableFoods.Length <= 0)
         {
-            Application.LoadLevel(1);
+            StartCoroutine(ChangeLevel());
         }
 	}
 }
---
+++ Assets/SecondLevelLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondLevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Fading>().BeginFade(-1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
