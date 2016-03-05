af47818597cf46f2a8afd9cef28c768deb8b7692
Arturo Ordonez-Hernandez <Arturo Ordonez-Hernandez>
2016-03-05 13:53:28 -0700

    Added scoring system to painting minigame.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour
+{
+    int score;
+
+    GameObject rainbow;
+
+    void Start ()
+    {
+        score = 0;
+        rainbow = GameObject.Find("Rainbow");
+    }
+
+    void OnMouseDown()
+    {
+        UpdateScore();
+    }
+
+    void Update()
+    {
+    }
+
+    void UpdateScore()
+    {
+        Vector2 mouse2D = Camera.main.ScreenToWorldPoint(Input.mousePosition);       
+        RaycastHit2D hit = Physics2D.Raycast(mouse2D, new Vector2(transform.position.x, transform.position.y), 0.0f);
+
+        if (hit.collider)
+        {
+            if (hit.collider.GetComponent<SpriteRenderer>().color == Color.white)
+            {
+                score++;
+            }
+        }
+        if(Painting.coloredArcs == 7)
+        {
+            SceneManager.LoadScene(1);
+        }        
+    }
+}
---
+++ Assets/Scripts/SwitchColor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SwitchColor: MonoBehaviour
+{
+    GameObject pencilSet;
+    GameObject selected;
+    GameObject rainbow;
+
+    string[] colorNames;
+
+	// Use this for initialization
+	void Init ()
+    {
+        colorNames = new string[7];
+
+        colorNames[0] = "Red";
+        colorNames[1] = "Orange";
+        colorNames[2] = "Yellow";
+        colorNames[3] = "Green";
+        colorNames[4] = "Blue";
+        colorNames[5] = "Indigo";
+        colorNames[6] = "Violet";
+
+        rainbow = GameObject.Find("Rainbow");
+        pencilSet = GameObject.Find("Pencils");
+        selected = GameObject.Find("SelectedPencil");
+	}
+	
+    void Start()
+    {
+        Init();
+    }
+
+    void OnMouseDown()
+    {
+        Change();
+    }
+
+    void Change()
+    {
+       Vector2 mouse2D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       RaycastHit2D hit = Physics2D.Raycast(mouse2D, new Vector2(transform.position.x, transform.position.y), 0.0f);
+
+        if (hit.collider)
+        {
+            selected.transform.GetComponent<SpriteRenderer>().color = pencilSet.transform.Find(transform.parent.name).GetComponent<MeshRenderer>().material.color;
+        }
+    }
+}
