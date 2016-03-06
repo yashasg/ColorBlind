fb24c63fbe8b958987643f1346db9a16706795fe
Kun Cheng <kunchengsky@gmail.com>
2016-03-05 23:06:53 -0700

    Made a pressable key

--- Assets/Scripts/PianoKey.cs
+++ Assets/Scripts/PianoKey.cs
@@ -2,14 +2,35 @@
 using System.Collections;
 
 public class PianoKey : MonoBehaviour {
+    private bool isDown= false;
+    private Vector3 originalPos;
+    private Vector3 downPos;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+	    originalPos = GetComponent<Transform>().position;
+        downPos = new Vector3(originalPos.x, originalPos.y - 0.5f, originalPos.z);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (isDown && !Input.GetMouseButton(0))
+        {
+            SetKeyUp();
+        }
+    }
+
+    public void SetKeyDown()
+    {
+        if (isDown == false)
+        {
+            isDown = true;
+            GetComponent<Transform>().position = downPos;
+        }
+    }
+    public void SetKeyUp()
+    {
+        isDown = false;
+        GetComponent<Transform>().position = originalPos;
+    }
 }
