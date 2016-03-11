b183bef94f4238889412297a0520bfda28d7f658
yashasg <yashas.gujjar@gmail.com>
2016-03-10 19:44:07 -0700

    meesing with shaders right now so kun can change some parts of the school level

--- Assets/Crosshair.cs
+++ Assets/Crosshair.cs
@@ -1,20 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Crosshair : MonoBehaviour {
     public GameObject mainCamera;
     Texture crossHairTex;
     private Rect crossHairRect;
+    private Shader newShader;
+    public  Shader oldShader;
+    public GameObject kunsbitch;
 
 	// Use this for initialization
 	void Start () {
         float crossHairSize = Screen.width * 0.05f;
         crossHairTex = Resources.Load("crosshair") as Texture;
         crossHairRect = new Rect(Screen.width / 2 - crossHairSize / 2, Screen.height / 2 - crossHairSize / 2, crossHairSize, crossHairSize);
+        newShader = Shader.Find("Toon/Lit Outline");
+        oldShader = Shader.Find("Standard");
     }
-	
-	// Update is called once per frame
-	void OnGUI () {
+
+    // Update is called once per frame
+    /*void OnGUI () {
         GUI.DrawTexture(crossHairRect, crossHairTex);
         if (Input.GetMouseButtonDown(0))
         {
@@ -25,7 +31,11 @@ public class Crosshair : MonoBehaviour {
             // if the ray hits some thing
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.transform.tag == null || hit.transform.tag.ToString().Equals("Untagged"))
+                foreach( Material m in hit.transform.gameObject.GetComponentInChildren<MeshRenderer>().materials)
+                {
+                    m.shader = newshader;
+                }
+               /* if (hit.transform.tag == null || hit.transform.tag.ToString().Equals("Untagged"))
                Debug.Log(hit.transform.parent.tag);
                 else
                     Debug.Log(hit.transform.tag);
@@ -35,5 +45,49 @@ public class Crosshair : MonoBehaviour {
         {
             Debug.Log("Pressed Right click.");
         }
-	}
+	}*/
+    void Update()
+    {
+
+
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+            Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+            RaycastHit hit;
+            // if the ray hits some thing
+            if (Physics.Raycast(ray, out hit))
+            {
+            if (hit.transform.parent.parent.tag == "Yellow")
+            {
+                if (kunsbitch.gameObject.Equals(hit.transform.gameObject))
+                {
+
+                }
+                else
+                {
+                    resettheShaderonthisObject(kunsbitch);
+                    kunsbitch = hit.transform.gameObject;
+                    foreach (Material m in hit.transform.gameObject.GetComponentInChildren<MeshRenderer>().materials)
+                    {
+                        m.shader = newShader;
+                    }
+                }
+            }
+            else
+                Debug.Log(hit.transform.parent.tag);
+                /* if (hit.transform.tag == null || hit.transform.tag.ToString().Equals("Untagged"))
+                Debug.Log(hit.transform.parent.tag);
+                 else
+                     Debug.Log(hit.transform.tag);*/
+            }
+        }
+
+    private void resettheShaderonthisObject(GameObject kunsbitch)
+    {
+      foreach(Material m in kunsbitch.GetComponent<MeshRenderer>().materials)
+        {
+            m.shader = oldShader;
+        }
+    }
 }
+
