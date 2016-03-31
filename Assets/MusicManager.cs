e4fc1cecfabc8d4d1ef12dbb4ebddd458209cc30
milkticc <milkticc@gmail.com>
2016-03-31 10:49:26 -0600

    Alpha

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    private static GameObject instance;
+    public AudioSource music;
+    void Awake()
+    {
+        instance = GameObject.Find("MusicManager");
+
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        else 
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+       AudioSource playingMusic = GameObject.Find("MusicManager").gameObject.GetComponent<AudioSource>();
+        playingMusic.
+
+
+
+    }
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
